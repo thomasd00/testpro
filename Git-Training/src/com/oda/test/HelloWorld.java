1bf01256ae157a4297c57154b98bdf3dc0f7b187
Stefan, Amenda <stefan.amenda@gmail.com>
2021-02-16 15:02:52 +0100

    Stefan: Änderung

--- Git-Training/src/com/oda/test/HelloWorld.java
+++ Git-Training/src/com/oda/test/HelloWorld.java
@@ -7,6 +7,7 @@ public class HelloWorld {
 		System.out.println("Hello, World!");
 		System.out.println("new line.");
 		System.out.println("Another new line.");
+		System.out.println("Neue Zeile Stefan");
 
 	}
 
